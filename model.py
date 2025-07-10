3a04e01c99d0cc4d8205fefa67a0cf3cb0a3902d
TheCookieNoid <dhanushkarguin10@gmail.com>
2025-07-10 18:50:58 +0530

    Updates!

--- model.py
+++ model.py
@@ -6,6 +6,7 @@ X = dataset.iloc[:,:-1]
 Y = dataset.iloc[:,-1].values
 
 from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import StandardScaler 
 
 from sklearn.model_selection import train_test_split
@@ -13,7 +14,7 @@ X_train,X_test,Y_train,Y_test = train_test_split(X,Y, test_size=0.2,random_state
 
 pipeline = Pipeline([
     ('scaler', StandardScaler()),
-    ('logreg', LogisticRegression())
+    ('rfcl', RandomForestClassifier())
 ])
 
 pipeline.fit(X_train,Y_train)
